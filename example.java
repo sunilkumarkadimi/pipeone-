c48045994936d6652c05803bc625693e737d402f
sunilkumarkadimi <38122217+sunilkumarkadimi@users.noreply.github.com>
2021-03-26 10:41:29 -0700

    java file
    created java file

---
+++ example.java
@@ -0,0 +1,8 @@
+public class Exercise_01_09 {
+	public static void main(String[] args) {
+		System.out.println("Area = ");
+		System.out.println(4.5 * 7.9);
+		System.out.println("Perimeter = ");
+		System.out.println((4.5 + 7.9) * 2);
+	}
+}
